f898dc5916e3a01e65746b0f629e53437314e69f
yasamanhbn <hbn.yasaman@gmail.com>
2022-06-27 16:46:23 +0430

    adds file to colab

--- preprocessing.py
+++ preprocessing.py
@@ -1,46 +1,38 @@
-
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 import cv2
 import os
-from keras.preprocessing.image import ImageDataGenerator
 
-
-# Reading all images from dataset and labeling them
-
+train_dir = './base'
+Classes = ['closed', 'open']
+img_size = 128
 
 def create_training_data():
-    train_dir = '../dataset/base'
-    Classes = ['closed', 'open']
     training_data = []
     for category in Classes:
         path = os.path.join(train_dir, category)
         class_num = Classes.index(category)
         for img in os.listdir(path):
-            try :
-                img_array = cv2.imread(os.path.join(path,img))
-                training_data.append([img_array, class_num])
-            except Exception as e:
-                print(e)
+            img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+            backtorgb = cv2.cvtColor(img_array,cv2.COLOR_GRAY2RGB)
+            backtorgb  = cv2.resize(backtorgb, (img_size,img_size))
+            training_data.append([backtorgb, class_num])
+    return training_data
 
 def loadData():
-    train_dir = '../dataset/base'
-    img_size = 224
-    batch_size = 64
-    create_training_data()
-    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-    #test_datagen = ImageDataGenerator(rescale=1./255)
-    train_generator = train_datagen.flow_from_directory(
-        train_dir,      # This is the target directory
-        target_size=(img_size, img_size),    # All images will be resized to "img_size" * "img_size"
-        batch_size = batch_size,
-        class_mode='binary',           # Since we use binary_crossentropy loss, we need binary labels
-        subset='training')
+    X_train = []
+    y_label = []
+    training_data = create_training_data()
+    random.shuffle(training_data)
 
-    validation_generator = train_datagen.flow_from_directory(
-        train_dir, # same directory as training data
-        target_size=(img_size, img_size),
-        batch_size=batch_size,
-        class_mode='binary',
-        subset='validation') # set as validation data
+    for features, label in training_data:
+        X_train.append(features)
+        y_label.append(label)
 
-    return (train_generator, validation_generator)
+    X_train = np.array(X_train)
+    X_train = X_train/255.0
+    y_label = np.array(y_label)
 
+    return train_test_split(X_train, y_label, test_size=0.2, random_state=42)
