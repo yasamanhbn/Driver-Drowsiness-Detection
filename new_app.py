95c1ef2cd01f0df07743055a0f4056adea33a043
yasamanhbn <hbn.yasaman@gmail.com>
2022-07-07 15:47:11 +0430

    adds alarm option's choosing

--- new_app.py
+++ new_app.py
@@ -9,6 +9,7 @@ import multiprocessing
 from playsound import playsound
 import time
 import tkinter as tk
+from tkinter import filedialog as fd
 import PIL.Image, PIL.ImageTk
 from datetime import datetime
 
@@ -23,38 +24,68 @@ class App:
         BGCOLOR = '#ccf5ff'
         self.window = window
         self.window.configure(bg=BGCOLOR)
+        self.alarmFilePath = ''
         ws = self.window.winfo_screenwidth() 
         hs = self.window.winfo_screenheight()
-        w = int(ws*0.85)
-        h = int(hs*0.8) 
+        w = int(ws)
+        h = int(hs * 0.9)
         self.window.geometry("{}x{}".format(w,h)) #set window size
         x = (ws/2) - (w/2)
-        y = (hs/2) - (h/2)
-        self.window.geometry('+%d+%d' % (x, y))
+        self.window.geometry('+%d+%d' % (x, 0))
         self.window.title(window_title)
         self.video_source = video_source
-        self.ok=False
+        self.ok = False
 
         # open video source (by default this will try to open the computer webcam)
         self.videoCap = VideoCapture(self.video_source)
 
         # Radio buttons
-        # radio_Frame = tk.Frame(window, bg='#ccf5ff')
-        # radio_Frame.pack(side='right')
+        radio_Frame = tk.Frame(window, bg='#ccf5ff')
+        radio_Frame.pack(side=tk.RIGHT, padx=10)
         values= ["600", "650", "700"]
         self.v = tk.IntVar(value=650)
         for val in values:
-            tk.Radiobutton(window, 
+            tk.Radiobutton(radio_Frame, 
                 text=val,
                 bg=BGCOLOR,
                 pady = 20, 
                 variable=self.v,
-                value=val).pack(side=tk.RIGHT)
+                value=val).pack()
             # self.radio1.grid(row = 0, column = 0)
 
+        alarm_Frame = tk.Frame(window, bg='#ccf5ff')
+        alarm_Frame.pack(side=tk.LEFT, padx=5, anchor="e")
+        # 
+        tk.Label(
+            alarm_Frame,
+            text="در این قسمت می توانید زنگ هشدار مورد نظر خود", 
+            bg='#ccf5ff').pack()
+            
+        tk.Label(
+            alarm_Frame, 
+            text="را انتخاب کنید. در صورتی که زنگی انتخاب نکنید", 
+            bg='#ccf5ff').pack()
+
+        tk.Label(
+            alarm_Frame, 
+            text="از زنگ هشدار پیش فرض استفاده خواهد شد", 
+            bg='#ccf5ff').pack()
+
+        tk.Button(
+            alarm_Frame, 
+            bg='#98e6e6', 
+            text='انتخاب فایل', 
+            bd=0,
+            command=self.openFile, 
+            height = 2, 
+            width = 8
+        ).pack(pady = 5)
+
         # Create a canvas that can fit the above video source size
-        self.canvas = tk.Canvas(window, width = w * 0.6, height = h * 0.9, bg=BGCOLOR)
+        self.canvas = tk.Canvas(window, width = w * 0.5, height = h * 0.7, bg=BGCOLOR)
         self.canvas.pack(side=tk.TOP)
+
+
 
         #
         start_cancel = tk.Frame(window, bg=BGCOLOR)
@@ -62,17 +93,15 @@ class App:
 
         #video control buttons
 
-        self.btn_start=tk.Button(start_cancel, bd=0, bg='#98e6e6', text='START', command=self.open_camera, height = 5, width = 10)
-        self.btn_start.pack(side=tk.LEFT, padx=10,)
+        self.btn_start=tk.Button(start_cancel, bd=0, bg='#98e6e6', text='START', command=self.open_camera, height = 3, width = 7)
+        self.btn_start.pack(side=tk.LEFT, padx=10)
 
-        self.btn_stop=tk.Button(start_cancel,bd=0, bg='#98e6e6', text='STOP', command=self.close_camera, height = 5, width = 10)
-        self.btn_stop.pack(side=tk.LEFT, padx=10,)
+        self.btn_stop=tk.Button(start_cancel,bd=0, bg='#98e6e6', text='STOP', command=self.close_camera, height = 3, width = 7)
+        self.btn_stop.pack(side=tk.LEFT, padx=10)
 
         # quit button
-        self.btn_quit=tk.Button(start_cancel,bd=0, bg='#98e6e6', text='QUIT', command=sys.exit, height = 5, width = 10)
+        self.btn_quit=tk.Button(start_cancel,bd=0, bg='#98e6e6', text='QUIT', command=sys.exit, height = 3, width = 7)
         self.btn_quit.pack(side=tk.LEFT,padx=10)
-
-
 
 
         # After it is called once, the update method will be automatically called every delay milliseconds
@@ -80,6 +109,8 @@ class App:
         self.window.mainloop()
 
 
+    def openFile(self):
+        self.alarmFilePath = fd.askopenfilename()
     def open_camera(self):
         self.ok = True
         self.videoCap.prev = datetime.now()
@@ -95,12 +126,12 @@ class App:
     def update(self):
         # Get a frame from the video source
         if self.ok:
-            ret, frame = self.videoCap.get_frame(self.v.get())
+            ret, frame = self.videoCap.get_frame(self.v.get(), self.alarmFilePath)
 
             if ret:
                 self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
                 self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
-            self.window.after(self.delay,self.update)
+            self.window.after(self.delay, self.update)
 
 
 class VideoCapture:
@@ -110,10 +141,10 @@ class VideoCapture:
             raise ValueError("Unable to open video source", video_source)
         self.score = False
         self.img_size = 128
-        self.model = load_model(os.path.join(THIS_FOLDER, 'cnnBasic.h5'))
-        self.faceCascade = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'haarcascade_frontalface_default.xml'))
-        self.eye_cascade_main = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'haarcascade_eye.xml'))
-        self.eye_cascade_backup = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'haarcascade_righteye_2splits.xml'))
+        self.model = load_model(os.path.join(THIS_FOLDER, 'data/cnnBasic.h5'))
+        self.faceCascade = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'data/haarcascade_frontalface_default.xml'))
+        self.eye_cascade_main = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'data/haarcascade_eye.xml'))
+        self.eye_cascade_backup = cv2.CascadeClassifier(os.path.join(THIS_FOLDER, 'data/haarcascade_righteye_2splits.xml'))
         self.prev = datetime.now()
 
     def preprocessing(self, frame, roi_gray,roi_color, score):
@@ -148,7 +179,7 @@ class VideoCapture:
             return True
 
         # To get frames
-    def get_frame(self, threshold):
+    def get_frame(self, threshold, alarmFilePath):
         if self.cap.isOpened():
             cur_time = datetime.now()
             ret, frame = self.cap.read()
@@ -172,7 +203,11 @@ class VideoCapture:
                         print("sleep")
                         self.prev = datetime.now()
                         self.score = False
-                        playsound(os.path.join(THIS_FOLDER, 'alarm.wav'))
+                        if alarmFilePath:
+                            playsound(alarmFilePath)
+                        else:
+                            playsound(os.path.join(THIS_FOLDER, 'data/alarm.wav'))
+
                     break
                 # Return a boolean success flag and the current frame converted to BGR
                 return (ret, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
